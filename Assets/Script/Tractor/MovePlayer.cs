289104000206bf02ac1bc4a74f8a50c674be3c61
Mouhamed-13 <mouhgta14@gmail.com>
2022-10-20 12:41:10 +0200

    {Add} Structure project and {Update} Move Player

---
+++ Assets/Script/Tractor/MovePlayer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float speed = 6f;
+    public float jumpSpeed = 8f;
+    public float gravity = 20f;
+    private Vector3 moveDirection =Vector3.zero;
+    CharacterController Cc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Cc.isGrounded){
+            moveDirection = new Vector3(0   ,0,Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * Time.deltaTime * speed * 10);
+        Cc.Move(moveDirection * Time.deltaTime);
+    }
+}
