ede291ccafa13aaff4671a07ea84a7e03341974a
VixyLeDieu <tvv3show@gmail.com>
2022-10-24 17:12:02 +0200

    {Update} Post Process & materials
    and the grass script but

--- Assets/Script/Grass.cs
+++ Assets/Script/Grass.cs
@@ -10,8 +10,10 @@ public class Grass : MonoBehaviour
         float rnd = Random.Range(0, 360);
         transform.Rotate(new Vector3(0f,rnd,0f),Space.Self);
 
-        rnd = Random.Range(1f, 1.5f);
-        transform.localScale = new Vector3(rnd, rnd, rnd);
+        rnd = Random.Range(-0.2f, 0.2f);
+        float xScale = transform.localScale.x;
+        transform.localScale = new Vector3(xScale+rnd, xScale+rnd, xScale+rnd);
+
     }
 
     // Update is called once per frame
