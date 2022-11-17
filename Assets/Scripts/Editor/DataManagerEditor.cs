78b70691da5929fdd331867622390e6dd7a0a853
Kiryonn <60699390+Kiryonn@users.noreply.github.com>
2022-11-17 09:48:51 +0100

    converting spaces to tabs saves 3 bytes per indent

--- Assets/Scripts/Editor/DataManagerEditor.cs
+++ Assets/Scripts/Editor/DataManagerEditor.cs
@@ -6,14 +6,14 @@ using UnityEditor;
 [CustomEditor(typeof(DataManager), true)]
 public class DataManagerEditor : Editor
 {
-    public override void OnInspectorGUI()
-    {
-        DataManager dataManager = (DataManager)target;
+	public override void OnInspectorGUI()
+	{
+		DataManager dataManager = (DataManager)target;
 
-        if (GUILayout.Button("Save Json"))
-            dataManager.UpdateQuestions();
-            Debug.Log("I used to be a function, but I took an arrow to my script");
+		if (GUILayout.Button("Save Json"))
+			// dataManager.UpdateQuestions();
+			Debug.Log("I used to be a function, but I took an arrow to my script");
 
-        DrawDefaultInspector();
-    }
+		DrawDefaultInspector();
+	}
 }
