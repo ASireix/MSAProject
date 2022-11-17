6f98126b5fdd85ce389189bcd9d5d4b7dd95601c
Kiryonn <60699390+Kiryonn@users.noreply.github.com>
2022-11-17 09:45:59 +0100

    convert indentation to tab, sorted the files

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -6,38 +6,32 @@ using System.IO;
 
 public class DataManager : MonoBehaviour
 {
-    public Dictionary<string, Dictionary<string, Question[]>> questions;
-    string caca;
-    // Start is called before the first frame update
-    void Start() {
-        string json = ReadFromFile("QData.json");
-        questions = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, Question[]>>>(json);
-    }
+	public Dictionary<string, Dictionary<string, Question[]>> questions;
+	string caca;
+	// Start is called before the first frame update
+	void Start() {
+		string json = ReadFromFile("QData.json");
+		questions = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, Question[]>>>(json);
+	}
 
-    string ReadFromFile(string fileName) {
-        string path = Application.streamingAssetsPath + "/" + fileName;
-        if (File.Exists(path)) {
-            using (StreamReader reader = new StreamReader(path)) {
-                string json = reader.ReadToEnd();
-                return json;
-            }
-        }
-        else {
-            Debug.Log("No such file");
-        }
+	string ReadFromFile(string fileName) {
+		string path = Application.streamingAssetsPath + "/" + fileName;
+		if (File.Exists(path)) {
+			using (StreamReader reader = new StreamReader(path)) {
+				string json = reader.ReadToEnd();
+				return json;
+			}
+		}
+		else {
+			Debug.Log("No such file");
+		}
 
-        return "";
-    }
+		return "";
+	}
 
-    public void SaveJson(string fileName)
-    {
-        string path = Application.streamingAssetsPath + "/" + fileName;
-        string content = JsonConvert.SerializeObject(questions);
-        File.WriteAllText(path, content);
-    }
-
-    public void UpdateQuestions()
-    {
-        // change the "question" dictionary
-    }
+	public void SaveJson(string fileName) {
+		string path = Application.streamingAssetsPath + "/" + fileName;
+		string content = JsonConvert.SerializeObject(questions);
+		File.WriteAllText(path, content);
+	}
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,39 +4,39 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public enum Difficulty{ Particuliers, Exploitant };
-    string currentDifficulty;
-    string currentCategorie;
-    public enum Categorie { Viticulture, Élevage, Cultures, Jardins_Espaces_Verts };
+	public enum Difficulty{ Particuliers, Exploitant };
+	string currentDifficulty;
+	string currentCategorie;
+	public enum Categorie { Viticulture, Élevage, Cultures, Jardins_Espaces_Verts };
 
-    public Difficulty difficulty;
-    public Categorie categorie;
-    public static GameManager instance;
-    public DataManager dataManager;
+	public Difficulty difficulty;
+	public Categorie categorie;
+	public static GameManager instance;
+	public DataManager dataManager;
 
-    public int currentAnswer;
+	public int currentAnswer;
 
 
-    private void Awake() {
-        if (instance != null && instance != this) Destroy(this);
-        else instance = this;
-    }
+	private void Awake() {
+		if (instance != null && instance != this) Destroy(this);
+		else instance = this;
+	}
 
-    void Start()
-    {
-        currentAnswer = 0;
-        currentDifficulty = Difficulty.Particuliers.ToString();
-        currentCategorie = Categorie.Viticulture.ToString();
-        dataManager = GetComponent<DataManager>();
-        DontDestroyOnLoad(gameObject);
-    }
+	void Start()
+	{
+		currentAnswer = 0;
+		currentDifficulty = Difficulty.Particuliers.ToString();
+		currentCategorie = Categorie.Viticulture.ToString();
+		dataManager = GetComponent<DataManager>();
+		DontDestroyOnLoad(gameObject);
+	}
 
-    public DataManager GetDataManager() {
-        return dataManager;
-    }
+	public DataManager GetDataManager() {
+		return dataManager;
+	}
 
-    public Question[] GetQuestions()
-    {
-        return dataManager.questions[currentDifficulty][currentCategorie];
-    }
+	public Question[] GetQuestions()
+	{
+		return dataManager.questions[currentDifficulty][currentCategorie];
+	}
 }
